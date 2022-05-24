5eab5b37e90479b9c7b0750ed66407465f23f144
willkand <will.kirkland@codeimmersives.com>
2022-05-24 11:49:40 -0400

    2nd Commit

--- what-can-you-do.js
+++ what-can-you-do.js
@@ -1,7 +1,15 @@
 const prompt = require('prompt-sync')({sigint: true});
 
-let num1 = Number(prompt("Hello. Enter your age: "));
+let age = Number(prompt("Hello. Enter your age: "));
 
-if (num1 > 16){
-    console.log("You can't drive. ")
-};
+if(age < 16){
+    console.log("You can't drive.")
+}else if(age === 16){
+    console.log("You can drive but not vote")
+}else if(age === 17){
+    console.log("You can drive but not vote")
+}else if(age <=24){
+    console.log("You can vote but not rent a car")
+}else if(age >=25){
+    console.log("You can do pretty much anything")
+}
